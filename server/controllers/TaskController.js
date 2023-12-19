b61c33481b911c91ec3138972fcda011a0d704e0
volhatsekalo <322196@uwr.edu.pl>
2023-12-19 22:51:07 +0100

    Task controller finished

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -3,11 +3,11 @@ import Task from '../models/Task.js';
 export const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find();
-        res.status(200).json(tasks);
+        return res.status(200).json(tasks);
     }
     catch (err) {
         console.log(err);
-        res.status(500).json({
+        return res.status(500).json({
             message: 'Nie udało się pobrać zadań',
         });
     }
@@ -27,22 +27,50 @@ export const createTask = async (req, res) => {
 
         await task.save();
 
-        res.status(200).json({ task });
+        return res.status(200).json({ task });
     }
     catch (err) {
         console.log(err);
-        res.status(500).json({
+        return res.status(500).json({
             message: 'Nie udało się dodać nowego zadania',
         });
     }
 }
 
-export const getTaskById = (req, res) => {
+export const getTaskById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const task = await Task.findById(id)
+            .then(task => {
+                if (!task) {
+                    return res.status(404).json({ message: 'Nie znaleziono zadania o podanym ID' });
+                }
+            });
 
+        return res.status(200).json({ task });
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: 'Nie udało się znaleźć zadania o podanym id',
+        });
+    }
 }
 
 export const updateTask = async (req, res) => {
-
+    try {
+        const { id } = req.params;
+        const { taskType, description, date, time, pet } = req.body;
+        const updatedTask = await Task.findByIdAndUpdate(id, { taskType, description, date, time, pet }, { new: true })
+            .then(updatedTask => {
+                if (!updatedTask) {
+                    return res.status(404).json({ message: 'Nie znaleziono zadania o podanym ID' });
+                }
+            })
+        return res.status(200).json({ message: 'Dane zadania zostały pomyślnie zaktualizowane', updatedTask });
+    }
+    catch (err) {
+        return res.status(500).json({ message: 'Nie udało się zaktualizować zadania' });
+    }
 }
 
 export const deleteTask = async (req, res) => {
@@ -63,7 +91,7 @@ export const deleteTask = async (req, res) => {
                 }
             })
     }
-    catch (error) {
-        res.status(500).json({ message: 'Nie udało się usunąć zadania' });
+    catch (err) {
+        return res.status(500).json({ message: 'Nie udało się usunąć zadania' });
     }
 }
